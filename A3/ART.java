e90a2847058d0f266f145d2cb552fb04b873dc32
bengunther1997 <31209364+bengunther1997@users.noreply.github.com>
2020-11-08 20:57:52 +1100

    Split ART into own file

--- A3/ART.java
+++ A3/ART.java
@@ -66,20 +66,8 @@ class ART {
         reader.close();
         return output;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-	private static ArrayList<String> generatePool() {
+    
+    private static ArrayList<String> generatePool() {
 		 //category -> choices -> expression
 		 String[] normalChar = {"","z","?"}; // literal chars    i
 		 String[] wordSymbol = {"","\\w","\\W"}; // \w \W        i
@@ -172,15 +160,6 @@ class ART {
 		return candidates;
 	}
 
-
-
-
-
-
-
-
-	
-	
 	private static void runART() {
 		int[] S = new int[categoriesChoices];
 		Arrays.fill(S, 0);
@@ -266,6 +245,7 @@ class ART {
 			}	
 		}
 	}
+}
 
 	
 	
@@ -476,69 +456,4 @@ class ART {
 	}
 */
 
-	// we can deal with error handling later
-	// for now main throws the exceptions and dies if something is wrong
-	public static void main(String args[]) throws IOException {
-
-		// Creating the pool of commands
-		/*grepPool = generatePool();
-
-		//generatePool currently produces an empty string as input which grep doesnt like.. so manually removing it here.
-		grepPool.remove(0);
 	
-		System.out.println("ART");
-		runART();
-		System.out.println("RT");
-		runRT();*/
-		
-		/*EVERYTHING IS TEMPORARILY COMMENTED OUT TO MAKE GUI TESTING EASIER*/
-		/*GUI DOES NOT WORK YET - JUST MAKING IT LOOK OK*/
-		
-		new GUIWindow();
-		
-		
-		
-		
-
-		/*
-		Notes
-			Some possible things to add to GUI.
-				Let user choose file path to grepv1 and grepv2
-				Let user choose how many candiates are generated
-				Let user define max runs
-				Let user define the names of the grep exe files
-				Let user define name and file path of testing file/directory
-				Let user do an RT comparison	
-		 */
-
-
-
-
-
-
-
-
-
-		
-		
-		
-
-		// keeping this for later use
-		/*
-		 * ART artInstance = new ART(); String test_case = "crinkled";
-		 * 
-		 * try {
-		 * 
-		 * String output_v1 = artInstance.feedInputGrepv1(test_case); String output_v2 =
-		 * artInstance.feedInputGrepv2(test_case); System.out.println(output_v1);
-		 * System.out.println(output_v2);
-		 * 
-		 * if (output_v1.equals(output_v2)) { System.out.println("They are equal"); }
-		 * 
-		 * } catch (IOException e) { System.out.println(e); }
-		 */
-
-	}
-
-
-}
